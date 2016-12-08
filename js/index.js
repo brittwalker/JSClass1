1b4e745da86efcb315cf5b90ffdc37820c5cf1e7
brittwalker <brittanywalker@Brittanys-MacBook-Pro.local>
2016-12-08 11:46:37 -0500

    adding bounce.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
-//var bounce = new Bounce();
-//bounce.scale({
-  //from: {x:0.1, y:0.1},
-//  to: {x:1, y:1}
-//});
+var bounce = new Bounce();
+bounce.scale({
+  from: {x:0.1, y:0.1},
+  to: {x:1, y:1}
+});
 
 
 $('#stopButton').click(function (){
-  $('#stopLight').addClass('red');
-  $('#slowLight').removeClass('yellow');
-  $('#goLight').removeClass('green');
-  //bounce.applyTo($('.animationtarget'));
+  $('#stopLight').addClass('red animationtarget');
+  $('#slowLight').removeClass('yellow animationtarget');
+  $('#goLight').removeClass('green animationtarget');
+  bounce.applyTo($('.animationtarget'));
 });
 
 $('#slowButton').click(function (){
-  $('#stopLight').removeClass('red');
-  $('#slowLight').addClass('yellow');
-  $('#goLight').removeClass('green');
-  //bounce.applyTo($('.animationtarget'));
+  $('#stopLight').removeClass('red animationtarget');
+  $('#slowLight').addClass('yellow animationtarget');
+  $('#goLight').removeClass('green animationtarget');
+  bounce.applyTo($('.animationtarget'));
 });
 
 $('#goButton').click(function (){
-  $('#stopLight').removeClass('red');
-  $('#slowLight').removeClass('yellow');
-  $('#goLight').addClass('green');
-  //bounce.applyTo($(".animationtarget"));
+  $('#stopLight').removeClass('red animationtarget');
+  $('#slowLight').removeClass('yellow animationtarget');
+  $('#goLight').addClass('green animationtarget');
+  bounce.applyTo($(".animationtarget"));
 });
